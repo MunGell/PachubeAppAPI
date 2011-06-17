68c23126a7dd8539432dede07b8fe9e7707912ae
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2011-06-17 14:30:43 +0400

    Config reader added

--- PachubeAPI.php
+++ PachubeAPI.php
@@ -2,4 +2,12 @@
 require('PachubeUserAPI.php');
 require('PachubeFeedAPI.php');
 require('PachubeDatastreamAPI.php');
+
+function getConfig($token)
+{
+	$url = "http://beta.apps.pachube.com/conf/$token";
+	$config = file_get_contents($url,false);
+	return json_decode($config);
+}
+
 ?>
